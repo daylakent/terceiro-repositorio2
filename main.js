5151d87a311045b6ca3dfbee368a2747ee34ea5e
daylakent <143607633+daylakent@users.noreply.github.com>
2025-06-23 08:41:50 -0300

    aula 5 - 23/06

--- main.js
+++ main.js
@@ -4,19 +4,37 @@ numeroSenha.textContent = tamanhoSenha;
 
 const botoes = document.querySelectorAll('.parametro-senha__botao');
 
-botoes[0].onclik = diminuiTamanho;
-botoes[1].onclik = aumentaTamanho;
+console.log(botoes);
+
+botoes[0].onclick = diminuiTamanho;
+botoes[1].onclick = aumentaTamanho;
 
 function diminuiTamanho(){
     if (tamanhoSenha > 1){
-        tamanhoSenha = tamanhoSenha - 1;
+    tamanhoSenha--;
     }
     numeroSenha.textContent = tamanhoSenha;
+    geraSenha();
 }
 
 function aumentaTamanho(){
-    if(tamanhoSenha < 20){
-        tamanhoSenha = tamanhoSenha + 1;
+    if (tamanhoSenha < 20){
+    tamanhoSenha++;
     }
     numeroSenha.textContent = tamanhoSenha;
+    geraSenha();
+}
+
+const campoSenha = document.querySelector('#campo-senha');
+const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+
+function geraSenha() {
+    let senha = '' ; 
+    for(let i=0; i < tamanhoSenha; i++) {
+        let numeroAleatorio = Math.random()*letrasMaiusculas.length;
+        numeroAleatorio = Math.floor(numeroAleatorio);
+        senha = senha + letrasMaiusculas[ numeroAleatorio];
+    }
+    campoSenha.value = letrasMaiusculas; 
 }
