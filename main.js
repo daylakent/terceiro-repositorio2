2309d6076f4a805e5d93d28acc490f492a262b41
daylakent <143607633+daylakent@users.noreply.github.com>
2025-07-07 09:00:13 -0300

    ajuste de erros do main

--- main.js
+++ main.js
@@ -61,13 +61,13 @@ function classificaSenha(tamanhoAlfabeto) {
     let entropia = tamanhoSenha*Math.log2(tamanhoAlfabeto);
     console.log(entropia);
     forcaSenha.classList.remove('fraca', 'media', 'forte');
-    if (entropia > 57){
+    if (entropia > 57 ){
         forcaSenha.classList.add('forte');
-    } else if (entropia > 35 && entropia <57) {
+    } else if (entropia>35 && entropia<57) {
         forcaSenha.classList.add('media');
-    }else if (entropia <= 35){
+    }else if (entropia<=35){
         forcaSenha.classList.add('fraca');
     }
     const valorEntropia = document.querySelector('.entropia');
-    valorEntropia.textContent = 2**Math.floor(entropia)/(100e6*60*60*24);
+    valorEntropia.textContent = 2**Math.floor(entropia)/(100e6*60*24);
 }
